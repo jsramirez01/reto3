5ba1d027d200636b35631324fecdd58c191abffb
jsramirez <jose.ramrez2.mt@correo.usa.edu.co>
2021-10-30 14:32:08 -0500

    se ajsuto la url del web reservaciones

--- src/main/java/reto3/appi/Web/ControladorReservaciones.java
+++ src/main/java/reto3/appi/Web/ControladorReservaciones.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/reservations")
+@RequestMapping("/api/Reservation")
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
 
 public class ControladorReservaciones {
