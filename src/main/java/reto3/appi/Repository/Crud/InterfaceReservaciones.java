791275f96c4194c8e891be7fff26116a72d1b626
jsramirez <jose.ramrez2.mt@correo.usa.edu.co>
2021-11-09 23:52:55 -0500

    Se ajusto el qry

--- src/main/java/reto3/appi/Repository/Crud/InterfaceReservaciones.java
+++ src/main/java/reto3/appi/Repository/Crud/InterfaceReservaciones.java
@@ -13,6 +13,6 @@ public interface InterfaceReservaciones extends CrudRepository<Reservaciones,Int
     public List<Reservaciones> findAllByStartDateAfterAndStartDateBefore(Date dateOne, Date dateTwo);
 
     // select clientId, count(*) as "total" from reservacion group by cliendId order by total desc;
-    @Query("SELECT c.client, COUNT(c.client) from Reservacion AS c group by c.client order by COUNT(c.client)DESC")
+    @Query("SELECT c.client, COUNT(c.client) from Reservaciones AS c group by c.client order by COUNT(c.client)DESC")
     public List<Object[]> countTotalReservationsByClient();
 }
